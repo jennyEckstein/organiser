7a14859b0b10e853216b06f2afa890c6ada076a0
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2019-04-15 21:09:02 -0400

    Added page links

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <p>This is the TodoList app v.1.0.0. It is part of React Course.</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
+
